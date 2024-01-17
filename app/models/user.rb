03a2ee322af954ea851888842be9a09f32c7e250
Jackson Pires <jackson.pires@gmail.com>
2024-01-16 21:17:36 -0300

    adiciona associacoes, validacoes e metodo de instancia no model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :user_courses
+  has_many :courses, through: :user_courses
+
+  validates :name, presence: true
+
+  def name_and_email
+    "#{name} - #{email} - #{Date.current}"
+  end
 end
